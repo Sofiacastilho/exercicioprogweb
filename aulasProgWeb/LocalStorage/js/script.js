ccca2a5735b0a16e25442c5cb4ba563b0b8c4485
Sofia Castilho <sofiarscastilho@gmail.com>
2021-10-26 18:10:28 -0300

    1

--- aulasProgWeb/LocalStorage/js/script.js
+++ aulasProgWeb/LocalStorage/js/script.js
@@ -1,14 +1,38 @@
-function adicionar (){
+var array = [];
+function adicionar(){
+    var musica = document.getElementById("musica").value;
+    array.push(musica);
+    mostrar();
+    
 
-    var nome = document.getElementById("nome").Value;
+}
+
+
+function mostrar(){
+    document.getElementById("divMusicas").innerHTML="";
+    for(var i = 0; i < array.length; i++){
+
+        document.getElementById("divMusicas").innerHTML += array[i] + "<br>";
+
+    }
+}
+
+
+var listaUsuarios = [];
+
+function adicionarUser(){
+    var nome = document.getElementById("nome").value;
     var sobrenome = document.getElementById("sobrenome").value;
+    var idade = document.getElementById("idade").value;
+    var email = document.getElementById("email").value;
 
     var usuario = [];
-
     usuario.push(nome);
     usuario.push(sobrenome);
+    usuario.push(idade);
+    usuario.push(email);
 
     listaUsuarios.push(usuario);
 
-    window.localStorage.setItem("usuarios", JSON.stringify(listaUsuarios));
+    window.localStorage.setItem("usuarios",JSON.stringify(listaUsuarios));
 }
